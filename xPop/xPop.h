55cd84b986bfdd04ff6a35ebb6992febcdd82a25
brux <brux@meican.com>
2015-11-26 12:13:22 +0800

    Added Xcode project

---
+++ xPop/xPop.h
@@ -0,0 +1,19 @@
+//
+//  xPop.h
+//  xPop
+//
+//  Created by brux on 11/26/15.
+//  Copyright © 2015 Brux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for xPop.
+FOUNDATION_EXPORT double xPopVersionNumber;
+
+//! Project version string for xPop.
+FOUNDATION_EXPORT const unsigned char xPopVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <xPop/PublicHeader.h>
+
+
